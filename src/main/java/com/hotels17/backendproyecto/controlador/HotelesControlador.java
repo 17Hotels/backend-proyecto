9f64231070f1e1964aefd7ce1849743972b8a96e
Josemi Lombide <joslombide@gmail.com>
2022-03-08 11:54:21 +0100

    Hacer el HotelesControlador

--- src/main/java/com/hotels17/backendproyecto/controlador/HotelesControlador.java
+++ src/main/java/com/hotels17/backendproyecto/controlador/HotelesControlador.java
@@ -1,7 +1,75 @@
 package com.hotels17.backendproyecto.controlador;
 
+import com.hotels17.backendproyecto.modelo.Foto;
+import com.hotels17.backendproyecto.modelo.Habitacion;
+import com.hotels17.backendproyecto.modelo.Hotel;
+import com.hotels17.backendproyecto.modelo.Valoracion;
+import com.hotels17.backendproyecto.servicio.Servicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/hoteles")
 public class HotelesControlador {
+
+    @Autowired
+    private Servicio servicio;
+
+    @GetMapping()
+    public List<Hotel> getHoteles() {
+        return servicio.getHoteles();
+    }
+
+    @GetMapping("/pais/{pais}")
+    public List<Hotel> getHotelesPorPais(@PathVariable String pais) {
+        return servicio.getHotelesPorPais(pais);
+    }
+
+    @GetMapping("/ciudad/{ciudad}")
+    public List<Hotel> getHotelesPorCiudad(@PathVariable String ciudad) {
+        return servicio.getHotelesPorCiudad(ciudad);
+    }
+
+    @GetMapping("/{idHotel}")
+    public ResponseEntity<Hotel> getHotel(@PathVariable Integer idHotel) {
+        Hotel hotel = servicio.getHotel(idHotel);
+
+        if (hotel == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(hotel);
+    }
+
+    @GetMapping("/{idHotel}/fotos")
+    public ResponseEntity<List<Foto>> getFotosHotel(@PathVariable Integer idHotel) {
+        Hotel hotel = servicio.getHotel(idHotel);
+        if (hotel == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(hotel.getFotos());
+    }
+
+    @GetMapping("/{idHotel}/habitaciones")
+    public ResponseEntity<List<Habitacion>> getHabitacionesHotel(@PathVariable Integer idHotel) {
+        Hotel hotel = servicio.getHotel(idHotel);
+        if (hotel == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(hotel.getHabitaciones());
+    }
+
+    @GetMapping("/{idHotel}/valoraciones")
+    public ResponseEntity<List<Valoracion>> getValoracionesHotel(@PathVariable Integer idHotel) {
+        Hotel hotel = servicio.getHotel(idHotel);
+        if (hotel == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(hotel.getValoraciones());
+    }
 }
